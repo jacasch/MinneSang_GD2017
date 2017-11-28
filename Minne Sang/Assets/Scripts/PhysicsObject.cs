a35401bbbdc4ca5b9f6d769ca665c2e29dc6a63a
Jan Carlo Schneider <junk.u.spam@gmail.com>
2017-11-28 09:22:51 +0100

    Started to Write Physics

---
+++ Minne Sang/Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour {
+    public float minGroundNormalY = 0.65f;
+    public float gravityModifier = 1f;
+
+    protected bool grounded = false;
+    protected Rigidbody2D rb2d;
+    protected Vector2 velocity;
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] rayBuffer = new RaycastHit2D[16];
+    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16);
+
+    protected const float minMoveDist = 0.001f;
+    protected const float shellRadius = 0.01f;
+
+    private void OnEnable()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Start () {
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+        contactFilter.useLayerMask = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate(){
+        grounded = false;
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+        Vector2 movement = Vector2.up * deltaPosition.y;
+        Move(movement);
+    }
+
+    private void Move(Vector2 movement)
+    {
+        float dist = movement.magnitude;
+        if (dist > minMoveDist) {
+            int count = rb2d.Cast(movement, contactFilter,rayBuffer, dist + shellRadius);
+            hitBufferList.Clear();
+            for (int i = 0; i < count; i++) {
+                hitBufferList.Add(rayBuffer[i]);
+            }
+
+            for (int i = 0; i < hitBufferList.Count; i++) {
+                Vector2 currentNormal = hitBufferList[i].normal;
+                if (currentNormal.y > minGroundNormalY) {
+                    grounded = true;
+                }
+            }
+        }
+
+
+        rb2d.position += movement;
+    }
+}
