c95e67af08156f9b1aa95cba11966a6c2d34f014
Jan Carlo Schneider <junk.u.spam@gmail.com>
2017-12-01 14:43:04 +0100

    After Fail Fix

--- Minne Sang/Assets/Scripts/Enemy/EnemyDance/DanceExplosion.cs
+++ Minne Sang/Assets/Scripts/Enemy/EnemyDance/DanceExplosion.cs
@@ -21,20 +21,4 @@ public class DanceExplosion : MonoBehaviour
             Destroy(gameObject);
         }
 	}
-<<<<<<< HEAD:Minne Sang/Assets/Scripts/DanceExplosion.cs
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            PlayerStats stats = collision.gameObject.GetComponent<PlayerStats>();
-            if (stats.dmgTimer >= stats.dmgCD)
-            {
-                stats.dmgTimer = 0;
-                stats.hp -= dmg;
-            }
-        }
-    }
-=======
->>>>>>> 8a0640239f6748a621bc9100720438da5e90377c:Minne Sang/Assets/Scripts/Enemy/EnemyDance/DanceExplosion.cs
 }
--- Minne Sang/Assets/Scripts/QuestSystem/TextBox.cs
+++ Minne Sang/Assets/Scripts/QuestSystem/TextBox.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TextBox : MonoBehaviour {
-
+    public Phrase phrase;
 	// Use this for initialization
 	void Start () {
 		
