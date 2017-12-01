993260a0d9846eb2f57dd7b3d10363f575ba6053
RiseBasti <romanvonrotz@outlook.com>
2017-12-01 14:57:52 +0100

    SAVE_GitError2

---
+++ Minne Sang/Assets/Scripts/QuestSystem/TextBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextBox : MonoBehaviour {
+    public Phrase phrase;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
