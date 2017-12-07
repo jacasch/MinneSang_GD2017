a9b43411a50ee86ca3d481b44a2267e43680dd6c
plippuner <pierre.lippuner@gmail.com>
2017-12-07 15:02:44 +0100

    Dialogues with Text Prefab

--- Minne Sang/Assets/Scripts/PlayerStun.cs
+++ Minne Sang/Assets/Scripts/PlayerStun.cs
@@ -5,35 +5,25 @@ using UnityEngine;
 public class PlayerStun : MonoBehaviour
 {
     int dir = 1;
-    float distPlayer = 2.5f;  //Front
+    float distPlayer = 2.25f;  //Front
     float stunTimer;
     float castDuration = 0.15f;
     public float castTime = 1f;
     float castTimer = 0;
-    public float repeatCD = 5f;
+    public float repeatCD = 0f;
     float repeatTimer = 0;
 
-    BoxCollider2D collider;
+    BoxCollider2D boxCollider;
 
     // Use this for initialization
     void Start ()
     {
-        collider = GetComponent<BoxCollider2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetAxis("Horizontal") < 0)
-        {
-            dir = -1;
-        }
-        else if (Input.GetAxis("Horizontal") > 0)
-        {
-            dir = 1;
-        }
-        transform.localPosition = new Vector3(distPlayer * dir, 0, 0);
-
         if (repeatTimer < 0)
         {
             if (Input.GetAxis("Stun") != 0)
@@ -43,13 +33,24 @@ public class PlayerStun : MonoBehaviour
                 {
                     stunTimer = castDuration;
                     repeatTimer = repeatCD;
-                    collider.enabled = true;
+                    boxCollider.enabled = true;
                     castTimer = 0;
                 }
             }
             else
             {
                 castTimer = 0;
+
+                if (Input.GetAxis("Horizontal") < 0)
+                {
+                    dir = -1;
+                }
+                else if (Input.GetAxis("Horizontal") > 0)
+                {
+                    dir = 1;
+                }
+                transform.localPosition = new Vector3(distPlayer * dir, 0, 0);
+                boxCollider.offset = new Vector2(0f * dir, 0);
             }
         }
         else
@@ -57,23 +58,12 @@ public class PlayerStun : MonoBehaviour
             if (stunTimer > 0)
             {
                 stunTimer -= Time.deltaTime;
-
             }
             else
             {
                 repeatTimer -= Time.deltaTime;
-                collider.enabled = false;
+                boxCollider.enabled = false;
             }
         }
-
-
-
-
-
-
-
-
-
-
     }
 }
