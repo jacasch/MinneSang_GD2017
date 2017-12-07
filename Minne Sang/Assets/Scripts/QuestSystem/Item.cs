e11993f1dbedfe9057cb2d8678967de94a5c2294
Jan Carlo Schneider <junk.u.spam@gmail.com>
2017-12-07 10:01:51 +0100

    Started to work on EnemyItem Drop

--- Minne Sang/Assets/Scripts/QuestSystem/Item.cs
+++ Minne Sang/Assets/Scripts/QuestSystem/Item.cs
@@ -2,11 +2,12 @@
 
 [System.Serializable]
 public class Item {
-    public Item(GameObject drop, string name) {
+    public GameObject drop;
+    public string name;
+
+    public Item(GameObject drop, string name)
+    {
         this.drop = drop;
         this.name = name;
     }
-
-    public GameObject drop;
-    public string name;
 }
