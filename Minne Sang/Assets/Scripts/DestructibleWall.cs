5cc691a41044c0e65e4e839c5066f35bce6142ab
RiseBasti <romanvonrotz@outlook.com>
2017-12-07 15:42:36 +0100

    DestructibleWall

---
+++ Minne Sang/Assets/Scripts/DestructibleWall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleWall : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "StunToEnemy")
+        {
+
+            //ParticleEffects?
+
+            Destroy(gameObject);
+        }
+    }
+}
