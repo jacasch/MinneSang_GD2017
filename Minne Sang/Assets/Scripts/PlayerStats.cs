8ef4062ed352bcb2f018d6e7ddc0f244ca55c783
Jan Carlo Schneider <junk.u.spam@gmail.com>
2017-12-07 10:02:10 +0100

    Merge

--- Minne Sang/Assets/Scripts/PlayerStats.cs
+++ Minne Sang/Assets/Scripts/PlayerStats.cs
@@ -12,7 +12,11 @@ public class PlayerStats : MonoBehaviour
     //Zeit bis erneut verwundbar nach eingegangenem Schaden
     public float dmgTimer = 0;
     public float dmgCD = 0.5f;
-
+    public float poetryTime = 10;
+    public float poetryCD = 5;
+    public float poetryCastTime = 3;
+    float poetryCasting = 0;
+    float poetryBuff = -10;
 
     void Start ()
     {
@@ -26,12 +30,38 @@ public class PlayerStats : MonoBehaviour
         }
         else
         {
+            poetry();
             if (dmgTimer>0)
             {
                 dmgTimer -= Time.deltaTime;
             }
         }
 	}
+
+    void poetry()
+    {
+        if(poetryBuff < -poetryCD)
+        {
+            if (Input.GetAxis("Poetry") != 0)
+            {
+                poetryCasting += Time.deltaTime;
+                print(poetryCasting);
+                if (poetryCasting >= poetryCastTime)
+                {
+                    poetryBuff = poetryTime;
+                }
+            }
+            else
+            {
+                poetryCasting = 0;
+            }
+        }
+        else
+        {
+            poetryBuff -= Time.deltaTime;
+            print("poetryBuff:" + poetryBuff);
+        }
+    }
 
     //Wenn der Spieler im DMG des Gegners steht und er verwundbar ist, bekommt er Schaden und wird kurzzeitig unverwundbar
     private void OnTriggerStay2D(Collider2D collision)
@@ -40,6 +70,35 @@ public class PlayerStats : MonoBehaviour
         {
             //Variables
 
+            EnemyDMG enemyDMG = collision.GetComponent<EnemyDMG>();
+            int dir = 1;
+
+            //Prüft ob Gegner bereits erneut Schaden verursachen kann
+            if (enemyDMG.timer < 0)
+            {
+                //DMG to player
+                hp -= enemyDMG.dmg;
+                print("HP: " + hp);
+
+                //UnverwundbarkeitsTimer
+                dmgTimer = dmgCD;
+                //print("Timer: " + dmgTimer);
+
+                //Knockback
+                if (collision.transform.position.x > transform.position.x)
+                {
+                    dir = -1;
+                }
+                GetComponent<PlayerController>().KnockBack(enemyDMG.knockback * dir);
+                //print("Knockback: " + enemyDMG.knockback);
+
+                //Timer bis Gegner erneut Schaden verursachen kann
+                enemyDMG.timer = enemyDMG.dmgTime;
+            }
+        }
+        if (dmgTimer <= 0 && poetryBuff <= 0 && collision.gameObject.tag == "PoetryDmgToPlayer")
+        {
+            //Variables
             EnemyDMG enemyDMG = collision.GetComponent<EnemyDMG>();
             int dir = 1;
 
