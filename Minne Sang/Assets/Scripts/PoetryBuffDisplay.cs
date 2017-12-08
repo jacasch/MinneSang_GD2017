4264bdbca0d01c1e3843886594989b3fedc99cce
RiseBasti <romanvonrotz@outlook.com>
2017-12-08 16:12:49 +0100

    PoetryBuff ParticleSystem

---
+++ Minne Sang/Assets/Scripts/PoetryBuffDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoetryBuffDisplay : MonoBehaviour
+{
+    ParticleSystem particleSystem;
+
+    PlayerStats playerStats;
+
+	// Use this for initialization
+	void Start ()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+        playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(playerStats.poetryBuff>0)
+        {
+            particleSystem.Play();
+        }
+        else
+        {
+            particleSystem.Stop();
+        }
+	}
+}
