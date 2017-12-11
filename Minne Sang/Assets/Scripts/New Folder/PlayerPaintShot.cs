2f5ac815e324c9700097f65b8823d5faf04b8e71
RiseBasti <romanvonrotz@outlook.com>
2017-12-11 12:59:42 +0100

    update

--- Minne Sang/Assets/Scripts/New Folder/PlayerPaintShot.cs
+++ Minne Sang/Assets/Scripts/New Folder/PlayerPaintShot.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class PlayerPaintShot : MonoBehaviour {
     public GameObject prefab;
     private PlayerGui pg;
+    private PlayerController pc;
+    public bool aiming = false;
 
 	// Use this for initialization
 	void Start () {
         pg = GetComponent<PlayerGui>();
+        pc = GetComponent<PlayerController>();
+        pc.aiming = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetButtonDown("Paint") && pg.skillLevel >= 2) {
+            pc.aiming = true;
+        }
+        
+        if (Input.GetButtonUp("Paint") && pg.skillLevel >= 2) {
             GameObject instance = Instantiate(prefab, transform.position, transform.rotation) as GameObject;
             instance.GetComponent<PaintShot>().direction = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical") + 0.1f, 0);
+            pc.aiming = false;
         }
 	}
 }
---
+++ Minne Sang/Assets/Scripts/QuestSystem/Chest.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chest : Npc
+{
+    public int relevantAct;
+    public Quest quest;
+    public Phrase encounterBeforeHeadMaster;
+
+    public string targetScene;
+
+    //public Phrase afterDropLine;
+    //public Item questDrop;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    public override void NextPhrase()
+    {
+        //if the player hasnt talked to the headmaster yet, tell him do do so;
+        if (player.GetComponent<PlayerQuestHandler>().activeAct == -1)
+        {
+            if (activePhraseIndex >= 1)
+                EndInteraction();
+            activePhrase = encounterBeforeHeadMaster;
+            activePhraseIndex++;
+        }
+        //check if we are ready to talk to this master
+        else if (relevantAct == player.GetComponent<PlayerQuestHandler>().activeAct)
+        {
+
+            { // weare ready to talk to this master.
+                //if we have not yet gotten the quest, get the quest.
+                if (player.GetComponent<PlayerQuestHandler>().activeQuest == "end")
+                {
+                    //if dialogue is over we will end the dialogue and give the quest
+                    if (activePhraseIndex >= quest.dialogue.Length)
+                    {
+                        player.GetComponent<PlayerQuestHandler>().activeQuest = quest.name;
+                        ending();
+                    }
+                    else
+                    { //else we progress in the dialogues
+                        activePhrase = quest.dialogue[activePhraseIndex];
+                        activePhraseIndex++;
+                    }
+                }
+                else
+                { //we are already doing the quest he gave us
+                    //if we have all the items do endDialogue
+                    
+                        //if dialogue is over we will end the dialogue and enter the masterylevel
+                        if (activePhraseIndex >= quest.endDialogue.Length)
+                        {
+                            //TODO GO TO NEXT LEVEL
+                            player.GetComponent<PlayerQuestHandler>().activeQuest = "end";
+                        ending();
+                    }
+                        else
+                        { //else we progress in the dialogues
+                            activePhrase = quest.endDialogue[activePhraseIndex];
+                            activePhraseIndex++;
+                        }
+                    
+
+
+                }
+            }
+        }
+    }
+
+    private void ending()
+    {
+        if (player.GetComponent<PlayerQuestHandler>().letters >= 4)
+        {
+            Debug.Log("Good Ending", gameObject);
+        } else
+        {
+            Debug.Log("Bad Ending", gameObject);
+        }
+        EndInteraction();
+    }
+}
