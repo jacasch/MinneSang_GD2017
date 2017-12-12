a0b0b4a33522f5994e3f9a561311ddf58546b4c2
RiseBasti <romanvonrotz@outlook.com>
2017-12-12 16:54:21 +0100

    enemies Update

--- Minne Sang/Assets/Animation/Enemies/Squid/SquidMouth.cs
+++ Minne Sang/Assets/Animation/Enemies/Squid/SquidMouth.cs
@@ -4,27 +4,66 @@ using UnityEngine;
 
 public class SquidMouth : MonoBehaviour
 {
+    float animTimer = 0;
+
     public GameObject objEnemy;
     EnemyPaint enemyPaint;
 
     SpriteRenderer mySprite;
+    public Material defaultMat;
+    public Material chameleonMat;
+
+    Animator animator;
 
     // Use this for initialization
     void Start ()
     {
 		enemyPaint = objEnemy.GetComponent<EnemyPaint>();
+        mySprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
+        if (enemyPaint.stealth)
+        {
+            mySprite.material = chameleonMat;
+        }
+        else
+        {
+            mySprite.material = defaultMat;
+        }
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (enemyPaint.dir < 0)
+        print(enemyPaint.dir);
+        if (enemyPaint.dir > 0 && !enemyPaint.stealth)
         {
-
+            mySprite.flipX = true;
         }
         else
         {
-
+            mySprite.flipX = false;
         }
-	}
+        if(enemyPaint.shootTimer<=0)
+        {
+            animator.SetBool("shoot", true);
+            animTimer = 0.1f;
+        }
+        if(animTimer<=0)
+        {
+            animator.SetBool("shoot", false);
+        }
+        else
+        {
+            animTimer -= Time.deltaTime;
+        }
+        if (enemyPaint.stealth)
+        {
+            mySprite.material = chameleonMat;
+        }
+        else
+        {
+            mySprite.material = defaultMat;
+        }
+    }
 }
