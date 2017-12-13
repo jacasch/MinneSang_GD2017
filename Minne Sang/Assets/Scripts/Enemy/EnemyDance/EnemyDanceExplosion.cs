392efa0bdd1f1b17c63900167545748c711e89de
RiseBasti <romanvonrotz@outlook.com>
2017-12-13 09:11:46 +0100

    explosion dancer update

--- Minne Sang/Assets/Scripts/Enemy/EnemyDance/EnemyDanceExplosion.cs
+++ Minne Sang/Assets/Scripts/Enemy/EnemyDance/EnemyDanceExplosion.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class EnemyDanceExplosion : MonoBehaviour
 {
     float timer = 0.5f;
+    int timeUntilDestroyed = 1;
+
+    CircleCollider2D bombColl;
 
     //MAIN-----------------------------------------------------------------------------------------------------------------
     void Start ()
     {
         gameObject.layer = 0;
+        bombColl = GetComponent<CircleCollider2D>();
 	}
 	
 	void Update ()
     {
         timer -= Time.deltaTime;
         if(timer<0)
+        {
+            bombColl.enabled = false;
+        }
+        if(timer < -timeUntilDestroyed)
         {
             Destroy(gameObject);
         }
