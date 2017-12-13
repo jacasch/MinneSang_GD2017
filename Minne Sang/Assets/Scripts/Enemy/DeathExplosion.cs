414c6d6b3419c07980f1f4df77e6597da0311905
RiseBasti <romanvonrotz@outlook.com>
2017-12-13 10:27:35 +0100

    death explosion ooze

---
+++ Minne Sang/Assets/Scripts/Enemy/DeathExplosion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathExplosion : MonoBehaviour
+{
+    public bool died = false;
+
+    ParticleSystem particleSystem1;
+    ParticleSystem particleSystem2;
+
+    // Use this for initialization
+    void Start ()
+    {
+        particleSystem1 = transform.GetChild(0).GetComponent<ParticleSystem>();
+        particleSystem2 = transform.GetChild(1).GetComponent<ParticleSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(died)
+        {
+            particleSystem1.Play();
+            particleSystem2.Play();
+            died = false;
+        }
+    }
+}
