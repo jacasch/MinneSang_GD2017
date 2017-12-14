c5731a8311d6d67b9d64040af2bb4a3a7b0b14e8
Jan Carlo Schneider <junk.u.spam@gmail.com>
2017-12-14 17:05:11 +0100

    Start Screen Added

---
+++ Minne Sang/Assets/Scripts/StartMenu/NewGame.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewGame : MonoBehaviour {
+
+    public GameObject playerPackage;
+    GameObject player;
+    PlayerQuestHandler pqh;
+    PlayerSpawnHandler psh;
+
+    private void OnMouseDown()
+    {
+        StartNewGame();
+    }
+
+    private void StartNewGame() {
+
+        GameObject temp = Instantiate(playerPackage, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0));
+        player = temp.transform.Find("player").gameObject;
+        pqh = player.GetComponent<PlayerQuestHandler>();
+        psh = player.GetComponent<PlayerSpawnHandler>();
+
+        pqh.activeAct = -1;
+        pqh.activeQuest = "mastered";
+        pqh.letters = 0;
+        pqh.letterSend = false;     
+        psh.targetScene = "Intro";
+        psh.targetSpawn = "MainHallDoor";
+
+        psh.Respawn();
+    }
+}
