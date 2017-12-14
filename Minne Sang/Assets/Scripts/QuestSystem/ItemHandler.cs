621dc328a17f9549f3f378ceb1e25e7d737be80a
Jan Carlo Schneider <junk.u.spam@gmail.com>
2017-12-14 23:58:16 +0100

    item pickup delay

--- Minne Sang/Assets/Scripts/QuestSystem/ItemHandler.cs
+++ Minne Sang/Assets/Scripts/QuestSystem/ItemHandler.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class ItemHandler : MonoBehaviour {
     public string name = "Item Template";
+
+    private float pickUpDelay = 1.5f;
+
+    private void Start()
+    {
+        transform.Find("trigger").gameObject.layer = 8;
+    }
+
+    private void Update()
+    {   if (pickUpDelay >= 0)
+        {
+            pickUpDelay -= Time.deltaTime;
+        }
+        else
+        {
+            transform.Find("trigger").gameObject.layer = 0;
+            print("layermoed");
+        }
+    }
+
     public void SetName(string name) {
         this.name = name;
     }
