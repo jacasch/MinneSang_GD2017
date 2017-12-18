81f5f212bf7d605177f5ad57a42fcc12b006aeda
RiseBasti <romanvonrotz@outlook.com>
2017-12-18 12:05:31 +0100

    sounds: sirene, breakObjects

--- Minne Sang/Assets/Scripts/DestructibleWall.cs
+++ Minne Sang/Assets/Scripts/DestructibleWall.cs
@@ -5,26 +5,44 @@ using UnityEngine;
 public class DestructibleWall : MonoBehaviour
 {
 
+    SpriteRenderer mySprite;
+    AudioSource audioSource;
+    BoxCollider2D boxCol;
+
+    float destroyTimer = 0.5f;
+
+    bool destroy = false;
+
 	// Use this for initialization
 	void Start ()
     {
-		
+        audioSource = GetComponent<AudioSource>();
+        mySprite = GetComponent<SpriteRenderer>();
+        boxCol = GetComponent<BoxCollider2D>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+        if(destroy)
+        {
+            destroyTimer -= Time.deltaTime;
+            if(destroyTimer<=0)
+            {
+                Destroy(gameObject);
+            }
+        }
 	}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "StunToEnemy")
         {
-
+            destroy = true;
             //ParticleEffects?
-
-            Destroy(gameObject);
+            audioSource.Play();
+            mySprite.enabled = false;
+            boxCol.enabled = false;
         }
     }
 }
